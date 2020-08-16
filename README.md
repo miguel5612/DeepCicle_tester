06f55c84e58ca8448ddb1c71d8d4e24b30c697ae
miguel5612 <miguelangelcu@ufps.edu.co>
2020-08-15 19:03:59 -0500

    First developed device 15.08.2020

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # DeepCicle_tester
-deep cycle battery tester
+deep cycle battery tester created using arduino and open source componentes
+# Components
+1. 12V @ 200A Battery.
+2. Ks0185 keyestudio ACS712-30A Current Sensor
+3. Arduino Uno
+4. 2 Resistances for Voltage divisor
+# Objective
+Discharge the C10 battery to test if are Ok internally.
+# Calculations
+200Ah -> C10 Categorie -> 20Ah on 10 hours.
+Resistance = Voltage/Current = 12/20 = 0.6
+Power = V^2/R = 12^2/0.6 = 240 Watts
+Its can be represented between:
+1 -> 0.6 Ohms 240 Watt resistance
+2 -> 1.2 Ohms 120 Watt resistance
+4 -> 2.4 Ohms 60 Watt resistance
+8 -> 4.8 Ohms 30 Watt resistance
+16 -> 9.6 Ohms 15 Watt resistance
+Or comercially i was found:
+14 Resistances 20 Ohms 20 watts = 1.76 Ohms 
+14 Resistances 20 Ohms 10 Watts = 1.76 Ohms
+Total = 0.88 Ohms
+Current = Volt/Res = 12 / 0.88 = 13.63 Ampers 
+Ah = 13.63 * 1 hour = 13.63 Ah
+Time = 200Ah/13.63Ah = 14.67 Hours if battery was OK
---
+++ src/src/src.ino
@@ -0,0 +1,57 @@
+#include <Wire.h>  //invoking I2Clibraries coming with Arduino
+#include <LiquidCrystal_I2C.h>
+
+#define pinCurrentSensor A0
+#define pinVoltageSensing A1
+
+LiquidCrystal_I2C lcd(0x27,16,2); 
+
+int temp, voltage;
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+    lcd.begin();
+    delay(100);
+    lcd.backlight();
+    Wire.begin();  //initializing I2C
+    lcd.clear(); //clear screen
+    delay(200);
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  float current = analogRead(pinCurrentSensor);
+  float v = analogRead(pinVoltageSensing);
+  
+  temp=current;
+  temp=temp-512;
+  current=temp/13.6;
+
+  temp = v;
+  voltage = (temp*14)/1024; // 1024 on Analog pin is 14 volts - Full charged battery  
+  
+  //lcd.clear(); //clear screen
+  // print out the value you read:
+  
+  delay(100);
+  Serial.print("voltage=");
+  Serial.print(voltage);
+  Serial.print(", "); //Comma separated values on serial port
+  lcd.setCursor(2,0);
+    lcd.print("V=");
+    lcd.print(voltage);
+    lcd.print(" V");
+  Serial.print("current=");
+  Serial.print(current);
+  Serial.println("A");
+  lcd.setCursor(0,1);
+    lcd.print("Current=");
+    lcd.setCursor(8,1);
+    lcd.print(current);
+    lcd.setCursor(15,1);
+    lcd.print(" A");
+  delay(300);        // delay in between reads for stability
+}
